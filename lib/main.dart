94e7142f74bf27536221f54045255fbe02b19b05
Vivek <vivek0094.cse19@chitkara.edu.in>
2020-09-13 01:15:55 +0530

    Added networking functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'Screens/MainScreen.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class AppBody extends StatefulWidget {
-  @override
-  _AppBodyState createState() => _AppBodyState();
-}
-
-class _AppBodyState extends State<AppBody> {
-  TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
-
-  void _handleSubmitted(String text) {
-    print(text);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final _height = MediaQuery.of(context).size.height -
-        MediaQuery.of(context).padding.top -
-        kToolbarHeight;
-    final _width = MediaQuery.of(context).size.width;
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(15.0)),
-            border: Border.all(
-              color: Colors.black,
-              width: 0.4,
-            ),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          width: _width * 0.7,
-          height: _height * 0.5,
-          child: TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-                border: InputBorder.none, hintText: 'Enter text...'),
-            style: TextStyle(
-              fontSize: _height * 0.03,
-            ),
-          ),
-        ),
-        SizedBox(
-          height: _height * 0.1,
-        ),
-        FlatButton(
-          onPressed: () => _handleSubmitted(_controller.text),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-            ),
-            width: _width * 0.55,
-            height: _height * 0.09,
-            child: Center(
-              child: Text(
-                'VERIFY',
-                style: TextStyle(fontSize: _height * 0.03, color: Colors.white),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
