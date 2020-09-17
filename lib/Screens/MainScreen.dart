cec2a3407b920db583453f4727b4f1ade481efb0
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-09-17 10:01:21 +0530

    added verification text (ui)

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -8,6 +8,7 @@ class AppBody extends StatefulWidget {
 
 class _AppBodyState extends State<AppBody> {
   TextEditingController _controller;
+  bool onVerifyClick = false;
 
   @override
   void initState() {
@@ -32,6 +33,34 @@ class _AppBodyState extends State<AppBody> {
         MediaQuery.of(context).padding.top -
         kToolbarHeight;
     final _width = MediaQuery.of(context).size.width;
+
+    Column verifyText({bool isFake, String percentage}) {
+      String text;
+      Color textColor;
+      if (isFake) {
+        text = 'Incorrect';
+        textColor = Colors.red;
+      } else {
+        text = 'Correct';
+        textColor = Colors.green;
+      }
+
+      return Column(
+        children: [
+          Text(
+            text,
+            style: TextStyle(fontSize: _height * 0.03, color: textColor),
+          ),
+          SizedBox(
+            height: _height * 0.015,
+          ),
+          Text(
+            percentage,
+            style: TextStyle(fontSize: _height * 0.03, color: textColor),
+          ),
+        ],
+      );
+    }
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -68,6 +97,20 @@ class _AppBodyState extends State<AppBody> {
         SizedBox(
           height: _height * 0.05,
         ),
+        if (onVerifyClick)
+          Container(
+            padding: EdgeInsets.only(bottom: _height * 0.05),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                verifyText(isFake: true, percentage: '50%'),
+                SizedBox(
+                  width: _width * 0.15,
+                ),
+                verifyText(isFake: false, percentage: '50%'),
+              ],
+            ),
+          ),
         Container(
           width: _width * 0.55,
           height: _height * 0.09,
@@ -76,7 +119,12 @@ class _AppBodyState extends State<AppBody> {
               borderRadius: BorderRadius.circular(15.0),
               side: BorderSide(color: Colors.red),
             ),
-            onPressed: () => _handleSubmitted(_controller.text),
+            onPressed: () {
+              setState(() {
+                onVerifyClick = true;
+              });
+              _handleSubmitted(_controller.text);
+            },
             color: Colors.pink,
             child: Text(
               'VERIFY',
