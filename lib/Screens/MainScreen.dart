de61e6d66c28718938d41fd8ea5eb613bcda6ded
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-09-17 05:54:53 +0530

    fixed ui (text field and button)

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -33,64 +33,58 @@ class _AppBodyState extends State<AppBody> {
         kToolbarHeight;
     final _width = MediaQuery.of(context).size.width;
 
-    return Flexible(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-              border: Border.all(
-                color: Colors.black,
-                width: 0.4,
-              ),
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            width: _width * 0.7,
-            height: _height * 0.4,
-            child: Flexible(
-              child: TextField(
-                maxLines: 8,
-                textAlignVertical: TextAlignVertical.bottom,
-                enableInteractiveSelection: true,
-                controller: _controller,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration.collapsed(
-                    border: InputBorder.none,
-                    hintText: 'Enter text...',
-                    hintStyle: TextStyle(fontSize: _height * 0.03)),
-                style: TextStyle(
-                  fontSize: _height * 0.03,
-                ),
-                scrollPhysics: BouncingScrollPhysics(
-                    parent: AlwaysScrollableScrollPhysics()),
-              ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            border: Border.all(
+              color: Colors.black,
+              width: 0.4,
             ),
           ),
-          SizedBox(
-            height: _height * 0.05,
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+          width: _width * 0.7,
+          height: _height * 0.5,
+          child: TextField(
+            maxLines: 200,
+            textAlignVertical: TextAlignVertical.bottom,
+            enableInteractiveSelection: true,
+            controller: _controller,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration.collapsed(
+                border: InputBorder.none,
+                hintText: 'Enter text...',
+                hintStyle: TextStyle(fontSize: _height * 0.03)),
+            style: TextStyle(
+              fontSize: _height * 0.03,
+            ),
+            scrollPhysics:
+                BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           ),
-          FlatButton(
+        ),
+        SizedBox(
+          height: _height * 0.05,
+        ),
+        Container(
+          width: _width * 0.55,
+          height: _height * 0.09,
+          child: FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              side: BorderSide(color: Colors.red),
+            ),
             onPressed: () => _handleSubmitted(_controller.text),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.pink,
-                borderRadius: BorderRadius.all(Radius.circular(15.0)),
-              ),
-              width: _width * 0.55,
-              height: _height * 0.09,
-              child: Center(
-                child: Text(
-                  'VERIFY',
-                  style:
-                      TextStyle(fontSize: _height * 0.03, color: Colors.white),
-                ),
-              ),
+            color: Colors.pink,
+            child: Text(
+              'VERIFY',
+              style: TextStyle(fontSize: _height * 0.03, color: Colors.white),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
