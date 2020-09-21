6d01aae6e661c1bcff87cfe63d13cc92dec49bf3
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-09-22 00:44:46 +0530

    trying to fix percentage calculator

--- lib/Utilities/Analyzer.dart
+++ lib/Utilities/Analyzer.dart
@@ -16,39 +16,86 @@ class Analyzer {
     List<String> title = [];
     List<String> link = [];
     List<String> url = [];
-    String wordset =
-        'falsehood lie forgery fraud phoney pirate false pseudo fakey cheat bluff fake viral hoax rumour';
-    for (int i = 0; i < 10; i++) {
-      snip.add(decodeJson1['items'][i]['snippet']);
-      snip.add(decodeJson2['items'][i]['snippet']);
-      title.add(decodeJson1['items'][i]['title']);
-      title.add(decodeJson2['items'][i]['title']);
-      link.add(decodeJson1['items'][i]['displayLink']);
-      link.add(decodeJson2['items'][i]['displayLink']);
-      url.add(decodeJson1['items'][i]['formattedUrl']);
-      url.add(decodeJson2['items'][i]['formattedUrl']);
-    }
+    String wordset = 'fake hoax rumour fabricated';
+    // snip.add(decodeJson1['items'][0]['snippet']);
     Rake rake = Rake();
+    // for (int i = 0; i < 10; i++) {
+    //   String word = decodeJson2['items'][i]['snippet'];
+    //   double percentage = wordFrequencySimilarity(
+    //       rake.rank(word.toString()).toString(), rake.rank(q).toString());
+    //   print('$i     $percentage');
+    // }
+    //
+    // // >= 0.30
+    // for (int i = 0; i < 10; i++) {
+    //   String word = decodeJson2['items'][i]['snippet'];
+    //   print('$i    ${rake.rank(word.toString())}');
+    //   print('');
+    // }
+
+    // for (int i = 0; i < 10; i++) {
+    //   String word = decodeJson2['items'][i]['title'];
+    //   double percentage = wordFrequencySimilarity(
+    //       rake.rank(word.toString()).toString(), rake.rank(q).toString());
+    //   print('$i     $percentage');
+    // }
+    //
+    // // >= 0.35  DOUBT
+    // for (int i = 0; i < 10; i++) {
+    //   String word = decodeJson2['items'][i]['title'];
+    //   print('$i    ${rake.rank(word.toString())}');
+    //   print('');
+    // }
+
+    // Display Link not required
+
+    for (int i = 0; i < 10; i++) {
+      String word = decodeJson1['items'][i]['formattedUrl'];
+      double percentage = wordFrequencySimilarity(
+          rake.rank(word.toString()).toString(), rake.rank(q).toString());
+      print('$i     $percentage');
+    }
+
+    // >= 0.30
+    for (int i = 0; i < 10; i++) {
+      String word = decodeJson1['items'][i]['formattedUrl'];
+      print('$i    ${rake.rank(word.toString())}');
+      print('');
+    }
+
+    // for (int i = 0; i < 10; i++) {
+    //   snip.add(decodeJson1['items'][i]['snippet']);
+    //   snip.add(decodeJson2['items'][i]['snippet']);
+    //   title.add(decodeJson1['items'][i]['title']);
+    //   title.add(decodeJson2['items'][i]['title']);
+    //   link.add(decodeJson1['items'][i]['displayLink']);
+    //   link.add(decodeJson2['items'][i]['displayLink']);
+    //   url.add(decodeJson1['items'][i]['formattedUrl']);
+    //   url.add(decodeJson2['items'][i]['formattedUrl']);
+    // }
+    // Rake rake = Rake();
+    // print('This is rake : ');
     // print(rake.rank(
     //     snip.join(" ") + title.join(" ") + link.join(" ") + url.join(" ")));
-    print(wordFrequencySimilarity(
-        rake
-            .rank(snip.join(" ") +
-                title.join(" ") +
-                link.join(" ") +
-                url.join(" "))
-            .toString(),
-        wordset));
-    int percentage = (wordFrequencySimilarity(
-                rake
-                    .rank(snip.join(" ") +
-                        title.join(" ") +
-                        link.join(" ") +
-                        url.join(" "))
-                    .toString(),
-                wordset) *
-            100)
-        .toInt();
-    return percentage;
+    // // print(wordFrequencySimilarity(
+    // //     rake
+    // //         .rank(snip.join(" ") +
+    // //             title.join(" ") +
+    // //             link.join(" ") +
+    // //             url.join(" "))
+    // //         .toString(),
+    // //     wordset));
+    // int percentage = (wordFrequencySimilarity(
+    //             rake
+    //                 .rank(snip.join(" ") +
+    //                     title.join(" ") +
+    //                     link.join(" ") +
+    //                     url.join(" "))
+    //                 .toString(),
+    //             wordset) *
+    //         100)
+    //     .toInt();
+    // return percentage;
+    return 0;
   }
 }
